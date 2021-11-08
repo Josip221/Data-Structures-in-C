a4d55aa2c035feb33c7be97e2e73705a81fb4cbf
Josip Maretić <josip.maretic2011@outlook.com>
2021-11-08 23:16:05 +0100

    Prepravio liste

--- Lab4/Zad4.c
+++ Lab4/Zad4.c
@@ -1,27 +1,32 @@
+#include "Funkcije.h"
+#include "Polynomial.c"
 #include<stdio.h>
 #include<stdlib.h>
-#include "Zad4.h"
-#define MAX 100
 
-int main(){
-	Position poly1 = NULL, poly2 = NULL, zbroj = NULL, umnozak = NULL;
+
+int main(int argc, char** argv){
+	Poly poly1 = {.baza = 0, .exp = 0, .next = NULL};
+	Poly poly2 = {.baza = 0, .exp = 0, .next = NULL};
+	Poly zbroj = {.baza = 0, .exp = 0, .next = NULL};
+	Poly umnozak = {.baza = 0, .exp = 0, .next = NULL};
 	char buff1[MAX], buff2[MAX];
 
-	poly1 = ProcitajDatoteku("polinom1.txt", buff1);
-	poly2 = ProcitajDatoteku("polinom2.txt", buff2);
+	ProcitajDatoteku("polinom1.txt", buff1, &poly1);
+	ProcitajDatoteku("polinom2.txt", buff2, &poly2);
 	printf("Polinom 1: ");
-	Ispis(poly1->next);
+	Ispis(poly1.next);
 	printf("Polinom 2: ");
-	Ispis(poly2->next);
+	Ispis(poly2.next);
 
-	zbroj = ZbrojiPolinome(poly1->next, poly2->next);
+	ZbrojiPolinome(poly1.next, poly2.next, &zbroj);
 	printf("Zbroj polinoma: ");
-	Ispis(zbroj->next);
+	Ispis(zbroj.next);
 
-	umnozak = PomnoziPolinome(poly1->next, poly2->next);
+	PomnoziPolinome(poly1.next, poly2.next, &umnozak);
 	printf("Umnozak polinoma: ");
-	Ispis(umnozak->next);
+	Ispis(umnozak.next);
 
+	// OslobodiMemoriju(poly1);
 	return 0;
 };
 
